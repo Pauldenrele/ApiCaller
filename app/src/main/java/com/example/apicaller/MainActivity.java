014b6ac1b76c5b1d72322644d2dfc90f178f0142
PAULI <adenreleademola@yahoo.com>
2019-05-01 14:30:14 +0100

    Updates

--- app/src/main/java/com/example/apicaller/MainActivity.java
+++ app/src/main/java/com/example/apicaller/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.apicaller;
 
+import android.app.VoiceInteractor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +21,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        String URL = "https://192.168.43.219:8081/RestWS/fetch/customer";
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
         JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Response " , response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+         Log.e("Error " , error.toString());
+            }
+        }
 
 
         );
